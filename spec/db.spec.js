06422fa9198671697b481dd1cbb04a318a86091f
kamituel <kamituel@gmail.com>
2013-09-12 08:46:34 +0200

    Array type support

--- spec/db.spec.js
+++ spec/db.spec.js
@@ -10,12 +10,41 @@ var expect = require('chai').expect
 	, Q = require('q')
 	;
 
+
 describe('Redis-oose tests:', function () {
+
 	var Vehicle = new Roose.Model('vehicle', {
 		'manufacturer': String,
 		'model': String,
 		'$date_of_production': Number,
-		'$production_line': String
+		'$production_line': String,
+		'colors': [String]
+	});
+
+	describe('Invalid models', function () {
+		it('No model name', function () {
+			expect(function () {
+				new Roose.Model({
+					'$name': String
+				});
+			}).to.throw(/Model name unspecified/);
+		});
+
+		it('No primary key', function () {
+			expect(function () {
+				new Roose.Model('test', {
+					'name': String
+				});
+			}).to.throw(/No primary key/);
+		});
+
+		it('Invalid array definition', function () {
+			expect(function () {
+				new Roose.Model('test', {
+					'$colors': [String, String]
+				});
+			}).to.throw(/Invalid field length/);
+		});
 	});
 
 	describe('Simple add/del', function () {
@@ -23,7 +52,8 @@ describe('Redis-oose tests:', function () {
 			'manufacturer': 'Tesla',
 			'model': 'Model S',
 			'date_of_production': new Date().getTime(),
-			'production_line': 'X2CDHA3'
+			'production_line': 'X2CDHA3',
+			'colors': ['red', 'green', 'blue']
 		});
 
 		it('Saves', function (done) {
@@ -33,6 +63,7 @@ describe('Redis-oose tests:', function () {
 				expect(teslaSaved.model).to.equal(tesla.model);
 				expect(teslaSaved.date_of_production).to.equal(tesla.date_of_production);
 				expect(teslaSaved.production_line).to.equal(tesla.production_line);
+				expect(teslaSaved.colors.sort()).to.deep.equal(tesla.colors.sort());
 				done();
 			});
 		});
@@ -48,6 +79,7 @@ describe('Redis-oose tests:', function () {
 				expect(teslaRead.model).to.equal(tesla.model);
 				expect(teslaRead.date_of_production).to.equal(tesla.date_of_production);
 				expect(teslaRead.production_line).to.equal(tesla.production_line);
+				expect(teslaRead.colors.sort()).to.deep.equal(tesla.colors.sort());
 				done();
 			});
 		});
@@ -78,7 +110,8 @@ describe('Redis-oose tests:', function () {
 			'manufacturer': 'Mercedes',
 			'model': 'SLS',
 			'date_of_production': new Date().getTime(),
-			'production_line': '76'			
+			'production_line': '76',
+			'colors': ['#ff0000', '#ffffff']	
 		});
 
 		var id = {
@@ -104,6 +137,8 @@ describe('Redis-oose tests:', function () {
 				expect(mercedesRead.model).to.equal(mercedes.model);
 				expect(mercedesRead.date_of_production).to.equal(mercedes.date_of_production);
 				expect(mercedesRead.production_line).to.equal(mercedes.production_line);
+				console.log('actual', mercedesRead.colors, 'expected', mercedes.colors);
+				expect(mercedesRead.colors.sort()).to.deep.equal(mercedes.colors.sort());
 
 				done();
 			});
@@ -118,5 +153,48 @@ describe('Redis-oose tests:', function () {
 				done();
 			});
 		});
+	});
+
+	describe('Array types', function () {
+		var Student = new Roose.Model('student', {
+			'$name': String,
+			'teachers': [String]
+		});
+
+		var s1 = {
+			name: 'Jorge Luis Borges',
+			teachers: ['Italo', 'Calvino']
+		};
+
+		it('Won\'t create mixed array', function () {
+			expect(function () {
+				Student.create({
+					name: 'Jorge Luis Borges',
+					teachers: ['Italo', 7]
+				});
+			}).to.throw(/teachers/);
+		});
+
+		it('Will create & save string array', function () {
+			Q.spawn(function* () {
+				var student;
+				expect(function () {
+					student = Student.create(s1);
+				}).to.not.throw();
+
+				var studentSaved = yield student.save();
+				expect(studentSaved).to.not.be.null;
+				expect(studentSaved.name).to.equal(s1.name);
+				expect(studentSaved.teachers.sort()).to.equal(s1.teachers.sort());
+
+				var studentRead = yield Student.get({name: 'Jorge Luis Borges'});
+				console.log(s1, studentRead);
+				expect(studentRead).to.not.be.null;
+				expect(studentRead.name).to.equal(s1.name);
+				expect(studentRead.teachers.sort()).to.deep.equal(s1.teachers.sort());
+			});
+		});
+
+
 	});
 });
